c0c4d644bb4e3ed6eafee412ba639620bdadfa60
Coriolando <coriolando.medeiros@gmail.com>
2024-08-27 21:37:39 -0300

    feat: adicionar opção de detalhamento para pagamentos - Incluídos métodos de detalhamento para parcelas com e sem entrada.- Atualizado para perguntar se o usuário deseja ver o detalhamento após mostrar o valor total.

--- casa_propria.rb
+++ casa_propria.rb
@@ -1,53 +1,116 @@
-puts "nome"
-nome = gets.chomp.capitalize
-puts "Valor do empréstimo"
-valor_emprestimo = gets.chomp.to_f
-puts "Quantidade de parcelas"
-numero_parcelas = gets.chomp.to_i
-puts "Juros mensal"
-juros_mensal = gets.chomp.to_f
+def detalhamento_com_entrada
+  entrada = $valor_emprestimo * 0.2
+
+  saldo_devedor = $valor_emprestimo - entrada
+  # Iterando para calcular cada parcela
+  $numero_parcelas.times do |indice|
+    amortizacao = saldo_devedor / $numero_parcelas
+    juros_parcela = saldo_devedor * $juros_mensal
+    total_parcela = amortizacao + juros_parcela
+    puts "O valor da #{indice + 1}ª parcela é R$ #{'%.2f' % total_parcela}"
+
+    $valor_total_pago += total_parcela
+  
+    # Atualiza o saldo devedor
+    saldo_devedor -= amortizacao
+  end
+end
+
+def com_entrada
+  entrada = $valor_emprestimo * 0.2
+
+  saldo_devedor = $valor_emprestimo - entrada
+  # Iterando para calcular cada parcela
+  $numero_parcelas.times do |indice|
+    amortizacao = saldo_devedor / $numero_parcelas
+    juros_parcela = saldo_devedor * $juros_mensal
+    total_parcela = amortizacao + juros_parcela
+
+    $valor_total_pago += total_parcela
+  
+    # Atualiza o saldo devedor
+    saldo_devedor -= amortizacao
+  end
+  puts ""
+  puts "#{$nome}"
+  puts "Valor total pago R$ #{'%.2f' % $valor_total_pago}"
+  puts "Valor da entrada: R$ #{'%.2f' % entrada}"
+  puts "Deseja ver o detalhamento?"
+  puts "1 - Sim | Enter - Não"
+  detalhamento = gets.chomp
+  if detalhamento == '1'
+    detalhamento_com_entrada
+  elsif detalhamento != '1'
+    puts "Saindo..."
+    return
+  end
+end
+
+def detalhamento_sem_entrada
+  saldo_devedor = $valor_emprestimo
+
+  # Iterando para calcular cada parcela
+  $numero_parcelas.times do |indice|
+    amortizacao = saldo_devedor / $numero_parcelas
+    juros_parcela = saldo_devedor * $juros_mensal
+    total_parcela = amortizacao + juros_parcela
+    puts "O valor da #{indice + 1}ª parcela é R$ #{'%.2f' % total_parcela}"
+
+    $valor_total_pago += total_parcela
+  
+    # Atualiza o saldo devedor
+    saldo_devedor -= amortizacao
+  end
+end
+
+def sem_entrada
+  saldo_devedor = $valor_emprestimo
+
+  # Iterando para calcular cada parcela
+  $numero_parcelas.times do |indice|
+    amortizacao = saldo_devedor / $numero_parcelas
+    juros_parcela = saldo_devedor * $juros_mensal
+    total_parcela = amortizacao + juros_parcela
+    $valor_total_pago += total_parcela
+    # Atualiza o saldo devedor
+    saldo_devedor -= amortizacao
+  end
+  puts ""
+  
+  puts "#{$nome}"
+  puts "Valor total pago R$ #{'%.2f' % $valor_total_pago}"
+  puts "Deseja ver o detalhamento?"
+  puts "1 - Sim | Enter - Não"
+  detalhamento = gets.chomp
+  if detalhamento == '1'
+    detalhamento_sem_entrada
+  elsif detalhamento != '1'
+    puts "Saindo..."
+    return
+  end
+end
+puts "Sistema de simulação de financiamrento"
+puts "              habitacional"
+puts
+print "nome: "
+$nome = gets.chomp.capitalize
+
+print "Valor do empréstimo: "
+$valor_emprestimo = gets.chomp.to_f
+
+print "Quantidade de parcelas: "
+$numero_parcelas = gets.chomp.to_i
+
+print "Juros mensal: "
+$juros_mensal = gets.chomp.to_f / 100  # Aqui já divide por 100
+
 puts "Vai haver entrada? '1 - sim : 2 - não'"
 opcao = gets.chomp
-juros_mensal = juros_mensal / 100
-valor_total_pago = 0
 
+$valor_total_pago = 0.0  # Certifique-se de inicializar como ponto flutuante
 
 if opcao == '1'
-  entrada = valor_emprestimo * 0.2
-
-  saldo_devedor = valor_emprestimo - entrada
-  # Iterando para calcular cada parcela
-  numero_parcelas.times do |indice|
-    amortizacao = saldo_devedor / numero_parcelas
-    juros_parcela = saldo_devedor * juros_mensal
-    total_parcela = amortizacao + juros_parcela
-    puts "O valor da #{indice + 1}ª parcela é R$ #{'%.2f' % total_parcela}"
-
-    valor_total_pago += total_parcela
-  
-    # Atualiza o saldo devedor
-    saldo_devedor -= amortizacao
-  end
-  puts ""
-  puts "#{nome}"
-  puts "Valor total pago #{'%.2f' % valor_total_pago}"
-  puts "Valor da entrada: #{'%.2f' % entrada}"
+  com_entrada
 elsif opcao == '2'
-  saldo_devedor = valor_emprestimo
-
-  # Iterando para calcular cada parcela
-  numero_parcelas.times do |indice|
-    amortizacao = saldo_devedor / numero_parcelas
-    juros_parcela = saldo_devedor * juros_mensal
-    total_parcela = amortizacao + juros_parcela
-    puts "O valor da #{indice + 1}ª parcela é R$ #{'%.2f' % total_parcela}"
-
-    valor_total_pago += total_parcela
-  
-    # Atualiza o saldo devedor
-    saldo_devedor -= amortizacao
-  end
-  puts ""
-  puts "#{nome}"
-  puts "Valor total pago #{'%.2f' % valor_total_pago}"
+  sem_entrada
 end
